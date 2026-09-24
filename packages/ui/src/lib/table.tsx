b423ae557d193878208e6e02e0703a6fa327984e
Kevin McCartney <hello@kevinmccartney.is>
2026-09-24 09:27:29 -0400

    fix: ui upgrade table props

--- packages/ui/src/lib/table.tsx
+++ packages/ui/src/lib/table.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react';
 import { cn } from 'cn';
 
-function Table({ className, ...props }: React.ComponentProps<'table'>) {
+export type TableProps = React.ComponentProps<'table'> & {
+  containerClassName?: string;
+  containerRef?: React.Ref<HTMLDivElement>;
+};
+
+function Table({ className, containerClassName, containerRef, ...props }: TableProps) {
   return (
-    <div data-slot="table-container" className="relative w-full overflow-x-auto">
+    <div
+      ref={containerRef}
+      data-slot="table-container"
+      className={cn('relative w-full overflow-x-auto', containerClassName)}
+    >
       <table
         data-slot="table"
         className={cn('w-full caption-bottom text-xs', className)}
